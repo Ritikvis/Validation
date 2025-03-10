3f4e0aa45988fe9529b541c91e787dad2fcfff6b
Ritik Vishwakarma <ritikvishwakarma739@gmail.com>
2025-03-10 13:02:35 +0530

    add

--- src/main/java/com/Validation/Validation/ValidationApplication.java
+++ src/main/java/com/Validation/Validation/ValidationApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class gValidationApplication {
+public class ValidationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ValidationApplication.class, args);
